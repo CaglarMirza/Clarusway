13a74f4780852ab54317866ff35c9e6b2cad0f85
Caglar <ahmetcmirza@gmail.com>
2021-04-18 02:36:50 +0200

    Password reminder

---
+++ assignments_python/Password_Reminder.py
@@ -0,0 +1,15 @@
+name = "joseph"
+input_name = input("Please enter your name : ")
+if input_name.lower().strip() == name:
+    print(f"Hello, {name.title()}! The password is :W@12")
+else:
+    print(f"Hello, {input_name.title()}! See you later.")
+
+
+
+# Task : Let's say; you left a message in the past that prints a password you need. To see the password you wrote, you need to enter your name and the program should recognize you.
+# Write a program that 
+
+# Takes the first name from the user and compares it to yours,
+# Then if the name the user entered is the same as yours, print out such as : "Hello, Joseph! The password is : W@12",
+# If the name the user entered is not the same as yours, print out such as : "Hello, Amina! See you later."
