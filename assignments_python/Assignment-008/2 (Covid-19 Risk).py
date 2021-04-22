b30a7ce923e8da9a12becc6df35de6e5256ad9b2
Caglar <ahmetcmirza@gmail.com>
2021-04-22 14:10:30 +0200

    draf

---
+++ assignments_python/Assignment-008/2 (Covid-19 Risk).py
@@ -0,0 +1,71 @@
+
+age=  input("Are you a cigarette addict older than 75 years old?").lower().strip()
+chronic = input("Do you have a severe chronic disease?").lower().strip()
+immune = input("Is your immune system too weak?").lower().strip()
+
+if age == "yes":
+    print('in risk')
+elif age == "no":
+    if chronic == "yes":
+        print('in risk')
+    elif chronic == "no":
+        if immune == "yes":
+            print("in risk")
+        elif immune == "no":
+             print("sen yasiyosun bu hayti")
+        else:
+            print('gecerli immue deger girin')
+    else:
+        print('gecerli chorinic deger girin')
+else:
+    print('gecerli age deger girin')
+
+############################
+
+# liste = [age, chronic, immune]
+# liste2 = []
+
+# for i in range(len(liste)):
+#     if liste[i] == "yes":
+#         liste2.insert(i, True)
+#     elif liste[i] == "no":
+#         liste2.insert(i, False)  
+
+# if len(liste2) != len(liste):
+#     print("lutfen tum degerleri dogru giriniz")
+# elif True in  liste2:
+#     print("You are in risky group")
+# else:
+#     print("You are not in risky group")
+# risk = liste2[0] or liste2[1] or liste2[2]
+
+###############################
+'''
+if age == "yes":
+    age = True
+elif age == "no":
+    age = False
+
+if chronic == "yes":
+    chronic = True
+elif chronic == "no":
+    chronic = False
+
+if immune == "yes":
+    immune = True
+elif immune == "no":
+    immune = False
+
+booldeger = [age, chronic, immune]
+bool1 = bool(age or chronic or immune)
+if bool1 == True:
+    print("You are in risky group")
+elif bool1== False:
+    print("You are not in risky group")
+
+print(booldeger)
+'''
+
+
+
+
