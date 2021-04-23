c6b418578499ecd07e98dfb757a43b297b7841f4
Caglar <ahmetcmirza@gmail.com>
2021-04-23 23:44:49 +0200

    Is it an Armstrong Number?

---
+++ assignments_python/Armstrong Number.py
@@ -0,0 +1,12 @@
+value = input("Please enter a integer number : ")
+list_sum = []
+if not (value.isnumeric()):
+    print(f" '{value}' is an invalid entry. Don't use non-numeric, float, or negative values!")
+else:
+    for i in list(value):
+        list_sum.append(int(i)**len(value))
+
+    if sum(list_sum) == int(value):
+        print(f"'{value}' is an Armstrong number")
+    else:
+        print(f"'{value}' is not an Armstrong number")
