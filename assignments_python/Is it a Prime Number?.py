e40c52a7ae694c3f38a5a949eab23041b8f7a118
Caglar <ahmetcmirza@gmail.com>
2021-04-28 00:52:56 +0200

    updated

--- assignments_python/Is it a Prime Number?.py
+++ assignments_python/Is it a Prime Number?.py
@@ -3,8 +3,8 @@ value = input("Please write a number : ")
 if  value.isnumeric():
     if int(value) == 1:
         print(f"{value} is not a prime number")
-    elif int(value) == 2:
-        print(f"{value} is a prime number")
+    # elif int(value) == 2:
+    #     print(f"{value} is a prime number")
     else:
         for i in range(2,int(value)):
             if (int(value)%i) == 0:
