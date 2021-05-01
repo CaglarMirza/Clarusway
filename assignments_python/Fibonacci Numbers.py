95fc1d29b82a4324e1722474c6d46ef16da3c6b8
Caglar <ahmetcmirza@gmail.com>
2021-05-01 23:21:05 +0200

    updated

--- assignments_python/Fibonacci Numbers.py
+++ assignments_python/Fibonacci Numbers.py
@@ -1,16 +1,19 @@
 
 liste = []
-
 for i in range(11):
-    if i<=1:
-        liste.append(i)
-    else:
-        liste.append(liste[i-2]+liste[i-1])
+    liste.append(1 if i<=1 else liste[i-2]+liste[i-1] )
+
 print(liste)
 
 
-# liste = []
-# for i in range(11):
-#     liste.append(1 if i<=1 else liste[i-2]+liste[i-1] )
 
+# liste = []
+
+# for i in range(11):
+#     if i<=1:
+#         liste.append(i)
+#     else:
+#         liste.append(liste[i-2]+liste[i-1])
 # print(liste)
+
+
